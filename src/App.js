2c9dd8749e310045fad5df5c4aa644448d1c671a
Max Valyakin <m.valyakin@ntr1x.com>
2019-06-12 15:48:07 +0500

    Create Messenger

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import Messenger from '@/components/Messenger/Messenger.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Messenger
+  }
+}
---
+++ src/components/Messenger/Messenger.vue
@@ -0,0 +1,3 @@
+<template src="./Messenger.htm"></template>
+<script src="./Messenger.js"></script>
+<style lang="scss" src="./Messenger.scss"></style>
